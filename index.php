b94b304c31416453f71e925d736aa30e78d7a357
Andrei Apanasik <andreyapanasik@gmail.com>
2022-04-24 12:56:10 +0200

    Create sample routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Router.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+
+Router::get('', 'DashboardController');
+Router::get('dashboard', 'DashboardController');
+Router::get('projects', 'ProjectsController');
+
+Router::run($path);
