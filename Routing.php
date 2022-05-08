f01f0c3d7b4e41377e2ba4780fa8cde9434b252e
Andrei Apanasik <andreyapanasik@gmail.com>
2022-05-08 17:56:03 +0200

    Add connection to heroku db

--- Routing.php
+++ Routing.php
@@ -1,29 +1,34 @@
 <?php
 
 require_once 'src/controllers/DefaultController.php';
+require_once 'src/controllers/TopicsController.php';
 
-class Routing {
+class Routing
+{
 
     public static array $routers;
 
-    public static function get($url, $controller) {
+    public static function get($url, $controller)
+    {
         self::$routers[$url] = $controller;
     }
 
-    public static function post($url, $controller) {
+    public static function post($url, $controller)
+    {
         self::$routers[$url] = $controller;
     }
 
-    public static function run($url) {
+    public static function run($url)
+    {
 
         $action = explode("/", $url)[0];
-        if(!array_key_exists($action, self::$routers)) {
+        if (!array_key_exists($action, self::$routers)) {
             die("Wrong url!");
         }
 
         $controller = self::$routers[$action];
         $object = new $controller;
-        $action = $action ? : "index";
+        $action = $action ?: "index";
         $object->$action();
 
     }
